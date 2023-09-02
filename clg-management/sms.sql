a3c778479d8d22a51cde1b2b9b02830ac93a20a9
nithin831 <120774890+nithin831@users.noreply.github.com>
2023-09-02 13:54:42 +0530

    sms.sql

--- clg-management/sms.sql
+++ clg-management/sms.sql
@@ -1,31 +1,17 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Aug 08, 2021 at 04:26 PM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 8.0.7
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+`
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `sms`
---
 
 -- --------------------------------------------------------
 
---
+
 -- Table structure for table `login`
---
+
 
 CREATE TABLE `login` (
   `s_no` int(11) NOT NULL,
@@ -34,18 +20,18 @@ CREATE TABLE `login` (
   `password` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
+
 -- Dumping data for table `login`
---
+
 
 INSERT INTO `login` (`s_no`, `name`, `email`, `password`) VALUES
 (1, 'Admin', 'admin@gmail.com', 'admin@123');
 
 -- --------------------------------------------------------
 
---
+
 -- Table structure for table `students`
---
+
 
 CREATE TABLE `students` (
   `roll_no` varchar(20) NOT NULL,
@@ -58,18 +44,16 @@ CREATE TABLE `students` (
   `remark` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
+
 -- Dumping data for table `students`
---
+
 
 INSERT INTO `students` (`roll_no`, `name`, `father_name`, `class`, `mobile`, `email`, `password`, `remark`) VALUES
 ('1DS19CS051', 'Godlee', 'Joy', 'B', '1232313123', 'godlee@gmail.com', 'godlee@123', '');
 
 -- --------------------------------------------------------
 
---
 -- Table structure for table `teachers`
---
 
 CREATE TABLE `teachers` (
   `s_no` int(11) NOT NULL,
@@ -81,48 +65,38 @@ CREATE TABLE `teachers` (
   'password' varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
+
 -- Dumping data for table `teachers`
---
 
 INSERT INTO `teachers` (`s_no`, `t_id`, `name`, `mobile`, `courses`,'email','password') VALUES
 (1, 101, 'darshan ', '9972727499', 'Physics, Maths','darshan@gmail.com',123);
 
 
 
---
--- Indexes for dumped tables
---
 
---
 -- Indexes for table `login`
---
+
 ALTER TABLE `login`
   ADD PRIMARY KEY (`s_no`);
 
---
+
 -- Indexes for table `teachers`
---
+
 ALTER TABLE `teachers`
   ADD PRIMARY KEY (`s_no`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
+
 -- AUTO_INCREMENT for table `login`
---
+
 ALTER TABLE `login`
   MODIFY `s_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
---
+
 -- AUTO_INCREMENT for table `teachers`
---
+
 ALTER TABLE `teachers`
   MODIFY `s_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=104;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
